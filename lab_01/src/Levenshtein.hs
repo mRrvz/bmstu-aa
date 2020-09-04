ba1d0627409db2895a1db926347cb85b8feec03e
Romanov Alexey <romanov.alexey2000@gmail.com>
2020-09-05 02:03:55 +0300

    some fixes

--- lab_01/src/Levenshtein.hs
+++ lab_01/src/Levenshtein.hs
@@ -7,10 +7,6 @@ module Levenshtein (
 type Matrix = [[Int]]
 type Distance = Int
 
-data EditInfo = EditInfo { matrix :: Matrix,
-                   distance :: Distance
-                 } deriving (Show)
-
 min3 :: Int -> Int -> Int -> Int
 min3 = (min .) . min
 
@@ -24,8 +20,8 @@ levenshteinRecursion s1 s2 = helper s1 s2 [[]]
 levenshteinIterative :: String -> String -> Matrix
 levenshteinIterative s1 s2 = reverse $ foldl
     (\mtr i -> if head mtr == [] then [[0..length s1]] else calcRow mtr i : mtr) [[]] [0..length s2]
-    where calcRow mtr i = foldl
-            (\row j -> row ++ if length row == 0 then [length mtr] else [
-                min3 (last row + 1) (head mtr !! j + 1) (head mtr !! (j - 1) +
-                if s1 !! (j - 1) == s2 !! (i - 1) then 0 else 1)]
+    where calcRow mtr i = foldl (\row j ->
+            row ++ if length row == 0 then [length mtr] else [
+                min3 (last row + 1) (head mtr !! j + 1) $ head mtr !! (j - 1) +
+                    if s1 !! (j - 1) == s2 !! (i - 1) then 0 else 1]
             ) [] [0..length s1]
